5c226b57f261802926e72c0c04e0c099d40ac2bc
Coteh <3276350+Coteh@users.noreply.github.com>
2022-03-24 13:50:32 -0400

    Update build script for new share.js location

--- build.sh
+++ build.sh
@@ -1,11 +1,14 @@
 #!/bin/sh
 
 mkdir -p build/src/storage
+mkdir -p build/src/share
 
 cp index.html ./build
 cp index.css ./build
 cp src/*.js ./build/src
 cp src/storage/index.js ./build/src/storage
 cp src/storage/browser.js ./build/src/storage
+cp src/share/index.js ./build/src/share
+cp src/share/browser.js ./build/src/share
 cp favicon.ico ./build
 cp words.txt ./build
