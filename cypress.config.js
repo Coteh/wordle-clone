5f981158b6413cc90194a9b10564631e05bc5b70
Coteh <3276350+Coteh@users.noreply.github.com>
2022-06-19 17:48:36 -0400

    Prevent screenshots and videos from being taken when running Cypress in GitHub Actions runner

--- cypress.config.js
+++ cypress.config.js
@@ -2,6 +2,10 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
     e2e: {
-        setupNodeEvents(on, config) {},
+        setupNodeEvents(on, config) {
+            // implement node event listeners here
+        },
     },
+    screenshotOnRunFailure: process.env.CI === undefined,
+    video: process.env.CI === undefined,
 });
