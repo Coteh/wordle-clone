e50bee6678d96bb72dff7faaaf825a7144d7e95e
Coteh <3276350+Coteh@users.noreply.github.com>
2022-07-09 13:47:57 -0400

    Handle feather icons and Sentry failing to load
    Feather icons will be replaced by text equivalents so that player can still play the game.
    
    If an unexpected error occurs, and Sentry is not loaded either, then the error dialog will still appear instead of it throwing an error before that point.
    
    Cypress has also been updated to latest version.

--- cypress/e2e/game/misc.cy.js
+++ cypress/e2e/game/misc.cy.js
@@ -50,4 +50,74 @@ describe("misc", () => {
 
         cy.wait(5000);
     });
+
+    it("should be able to be played if feather icons could not load", () => {
+        cy.intercept("https://unpkg.com/feather-icons", {
+            statusCode: 500,
+        });
+
+        cy.intercept("/words.txt", {
+            fixture: "words.txt",
+        });
+        cy.clearBrowserCache();
+        cy.reload();
+        cy.waitForGameReady();
+
+        // Can still make inputs
+
+        cy.get("body").type("a");
+        cy.keyboardItem("b").click();
+
+        cy.currentRow().inputCell(1).inputLetter().should("have.text", "a");
+        cy.currentRow().inputCell(2).inputLetter().should("have.text", "b");
+
+        // Substitute "icons" are placed on the page
+
+        cy.get(".help-link").should("contain.text", "?");
+        // NTS: cy.keyboardItem("backspace") won't work because it selects the feather element and feather isn't loaded
+        cy.get(".keyboard > .keyboard-row:last-child > .keyboard-item:last-child").should(
+            "contain.text",
+            "←"
+        );
+
+        cy.get(".help-link").click();
+
+        cy.get(".dialog button.close").should("contain.text", "X").click();
+
+        // Can still play through the game
+
+        cy.get("body").type("{enter}");
+
+        cy.contains("Not enough letters").should("be.visible");
+
+        cy.get("body").type("{backspace}");
+        cy.get("body").type("{backspace}");
+
+        cy.keyboardItem("t").click();
+        cy.keyboardItem("e").click();
+        cy.keyboardItem("a").click();
+        cy.keyboardItem("c").click();
+        cy.keyboardItem("h").click();
+        cy.keyboardItem("enter").click();
+        cy.contains("You win!").should("be.visible");
+    });
+
+    it("should not prevent an error dialog from being displayed if Sentry were to not be defined", () => {
+        cy.intercept(
+            {
+                hostname: "js.sentry-cdn.com",
+            },
+            {
+                statusCode: 500,
+            }
+        );
+        cy.intercept("/words.txt", {
+            statusCode: 500,
+        });
+
+        cy.clearBrowserCache();
+        cy.reload();
+
+        cy.contains("The following error occurred:").should("be.visible");
+    });
 });
