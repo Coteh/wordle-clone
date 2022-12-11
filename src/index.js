16e2631f6e2f4d95d583cbcf9cdd6ec51c0d2916
Coteh <3276350+Coteh@users.noreply.github.com>
2022-12-11 00:00:36 -0500

    Update to v1.1.6

--- src/index.js
+++ src/index.js
@@ -300,6 +300,23 @@ document.addEventListener("DOMContentLoaded", async () => {
         document.body.classList.add(HIGH_CONTRAST);
     }
 
+    const landscapeQuery = window.matchMedia("(orientation: landscape)");
+
+    const checkForOrientation = (mediaQueryEvent) => {
+        const md = new MobileDetect(window.navigator.userAgent);
+        if (mediaQueryEvent.matches && md.mobile()) {
+            document.getElementById("landscape-overlay").style.display = "block";
+        } else {
+            document.getElementById("landscape-overlay").style.display = "none";
+        }
+    };
+
+    landscapeQuery.addEventListener("change", function (event) {
+        checkForOrientation(event);
+    });
+
+    checkForOrientation(landscapeQuery);
+
     try {
         await initGame(eventHandler);
     } catch (e) {
