af1c0b26243ec330d404d2aea2139e03cd3a25ef
Coteh <3276350+Coteh@users.noreply.github.com>
2022-12-21 23:07:46 -0500

    Add step to build script to copy vendor scripts

--- scripts/build.sh
+++ scripts/build.sh
@@ -4,6 +4,7 @@ OUTPUT_DIR=./build
 
 mkdir -p $OUTPUT_DIR/src/storage
 mkdir -p $OUTPUT_DIR/src/share
+mkdir -p $OUTPUT_DIR/vendor
 mkdir -p $OUTPUT_DIR/images
 
 cp index.html $OUTPUT_DIR
@@ -13,6 +14,7 @@ cp src/storage/index.js $OUTPUT_DIR/src/storage
 cp src/storage/browser.js $OUTPUT_DIR/src/storage
 cp src/share/index.js $OUTPUT_DIR/src/share
 cp src/share/browser.js $OUTPUT_DIR/src/share
+cp vendor/*.js $OUTPUT_DIR/vendor
 cp images/*.png $OUTPUT_DIR/images
 cp favicon.ico $OUTPUT_DIR
 cp icon*.png $OUTPUT_DIR
