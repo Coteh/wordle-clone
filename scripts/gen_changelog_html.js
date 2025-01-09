1626a800a0ce7b71a0d862f8c12f267a66db7842
James Cote <3276350+Coteh@users.noreply.github.com>
2025-01-09 01:16:27 -0500

    Remove invalid second argument from marked.parse call

--- scripts/gen_changelog_html.js
+++ scripts/gen_changelog_html.js
@@ -5,9 +5,4 @@ const marked = require('marked');
 
 const changelog = fs.readFileSync('CHANGELOG.md', 'utf8');
 
-console.log(marked.parse(changelog, (err, html) => {
-    if (err) {
-        console.error(err);
-        process.exit(1);
-    }
-}));
+console.log(marked.parse(changelog));
