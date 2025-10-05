b367bc621b75c609ced320c48b07f1dd17f9634c
James Cote <3276350+Coteh@users.noreply.github.com>
2025-10-05 18:02:59 -0400

    Update sw.js to test app update dialog

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // TODO: Base the cache name off the app version and commit hash
-const CACHE_NAME = "wordle-clone-v1.4.0";
+const CACHE_NAME = "wordle-clone-v1.4.0-rev3";
 
 const addResourcesToCache = async (resources) => {
     const cache = await caches.open(CACHE_NAME);
